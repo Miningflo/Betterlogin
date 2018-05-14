ac48ad8c4ed95694e40673be231fa0643a66cdbe
Miningflo <florian.debosscher@gmail.com>
2018-05-14 20:57:44 +0200

    Add files via upload

--- API/v2/Code/init.php
+++ API/v2/Code/init.php
@@ -1,7 +1,7 @@
 <?php
 
 require("./connect.php");
-$conn = connect();
+$conn = connect("");
 
 $sql = "CREATE DATABASE users";
 $conn->query($sql);
@@ -10,9 +10,10 @@ $sql = "CREATE TABLE users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(30) NOT NULL,
 password VARCHAR(255) NOT NULL,
-email VARCHAR(256),
-dob DATE,
+email VARCHAR(256) NOT NULL,
+dob DATE NOT NULL,
 reg_date TIMESTAMP
 )";
-
+$conn = connect("users");
+$conn->query($sql);
 ?>
